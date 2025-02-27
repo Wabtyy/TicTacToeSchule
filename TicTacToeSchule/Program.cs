7481764ada3b3a95bddecfea6ff43ed13a367f4d
Wabtyy <109748192+Wabtyy@users.noreply.github.com>
2025-02-27 14:38:05 +0100

    Update Program.cs

--- TicTacToeSchule/Program.cs
+++ TicTacToeSchule/Program.cs
@@ -1,32 +1,69 @@
-string rawdata = "O;X;O;X;O;X;X;O;O".ToLower();
+string rawdata = "O;X; ;X; ;X; ;O; ".ToLower();
 string[] data = rawdata.Split(";");
 
-string x1 = "═╗ ╔═";
-string x2 = " ╚╦╝ ";
-string x3 = " ╔╩╗ ";
-string x4 = "═╝ ╚═";
+string oo = "         ";
 
-string o1 = " ╔═╗ ";
-string o2 = "╔╝ ╚╗";
-string o3 = "╚╗ ╔╝";
-string o4 = " ╚═╝ ";
+string x1 = "   ═╗ ╔═ ";
+string x2 = "    ╚╦╝  ";
+string x3 = "    ╔╩╗  ";
+string x4 = "   ═╝ ╚═ ";
+
+string o1 = "    ╔═╗  ";
+string o2 = "   ╔╝ ╚╗ ";
+string o3 = "   ╚╗ ╔╝ ";
+string o4 = "    ╚═╝  ";
 
 int counter = 0;
+int secondcounter = 0;
 for (int x = 0; x < 4; x++)
 {
     counter += 1;
 
-    for (int i = 0; i < 9; i++)
+    for (int i = 0; i < data.Length/3; i++)
     {
         switch (data[i])
         {
             case "x": print("x"); break;
             case "o": print("o"); break;
+            case " ": print(" "); break;
         }
     }
     Console.WriteLine();
 }
+counter = 0;
+Console.WriteLine("═══════════════════════════════════");
+for (int x = 0; x < 4; x++)
+{
+    counter += 1;
 
+    for (int i = data.Length / 3; i < data.Length/1.5; i++)
+    {
+        switch (data[i])
+        {
+            case "x": print("x"); break;
+            case "o": print("o"); break;
+            case " ": print(" "); break;
+        }
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("═══════════════════════════════════"); 
+counter = 0;
+for (int x = data.Length / 2 + 1; x < data.Length; x++)
+{
+    counter += 1;
+
+    for (int i = 6; i < 9; i++)
+    {
+        switch (data[i])
+        {
+            case "x": print("x"); break;
+            case "o": print("o"); break;
+            case " ": print(" "); break;
+        }
+    }
+    Console.WriteLine();
+}
 Console.WriteLine("");
 
 
@@ -34,6 +71,8 @@ Console.WriteLine("");
 
 string print(string str)
 {
+    secondcounter += 1;
+
     switch (str)
     {
         case "x":
@@ -41,6 +80,7 @@ string print(string str)
             if (counter == 2) { Console.Write(x2 + "  "); }
             if (counter == 3) { Console.Write(x3 + "  "); }
             if (counter == 4) { Console.Write(x4 + "  "); }
+            if (secondcounter != 3) { Console.Write("║"); } else { secondcounter = 0; }
             break;
 
         case "o":
@@ -48,6 +88,15 @@ string print(string str)
             if (counter == 2) { Console.Write(o2 + "  "); }
             if (counter == 3) { Console.Write(o3 + "  "); }
             if (counter == 4) { Console.Write(o4 + "  "); }
+            if (secondcounter != 3) { Console.Write("║"); } else { secondcounter = 0; }
+            break;
+
+        case " ":
+            if (counter == 1) { Console.Write("           "); }
+            if (counter == 2) { Console.Write("           "); }
+            if (counter == 3) { Console.Write("           "); }
+            if (counter == 4) { Console.Write("           "); }
+            if (secondcounter != 3) { Console.Write("║"); } else { secondcounter = 0; }
             break;
     }
     return "";
