066785206f94ea5fae5d0c373ed54fae1663b0e4
Wabtyy <109748192+Wabtyy@users.noreply.github.com>
2025-02-28 13:45:28 +0100

    Update Program.cs

--- TicTacToeSchule/Program.cs
+++ TicTacToeSchule/Program.cs
@@ -1,37 +1,82 @@
-string rawdata = "o; ;o;o;x;x;X; ;O".ToLower();
-string[] data = rawdata.Split(";");
+
+
+using System.Security.Cryptography;
 
 string x1 = "   ═╗ ╔═ ",    o1 = "    ╔═╗  ";
 string x2 = "    ╚╦╝  ",    o2 = "   ╔╝ ╚╗ ";
 string x3 = "    ╔╩╗  ",    o3 = "   ╚╗ ╔╝ ";
 string x4 = "   ═╝ ╚═ ",    o4 = "    ╚═╝  ";
 
-int counter = 1, secondcounter = 0, one = 0, two = 3;
+int counter = 1, secondcounter = 0, one = 0, two = 3, three=-1, piece=0;
+string[] data = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+bool realgame = false;
 //////////////////////////////////////////////////////
-
-for (int z = 0; z < 4; z++, one+=3, two+=3, counter=1)
-{
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine("╠╬════════════╬════════════╬═══════════╬╣");
-
-    for (int x = 0; x < 4; x++, counter += 1)
+for (int c = 0; c < 13; c++, one = 0, two = 3, counter = 1, secondcounter = 0)
+{ if (three < 4) { three++; } else { Thread.Sleep(c * 35); }
+    
+    
+    if (c < 12)
     {
-        for (int i = one; i < two; i++)
+        Random rnd = new Random();
+        for (int t = 0; t < 9; t++)
         {
-            try
+            int a = three < 4 ? 1 : rnd.Next(1, 4);
+            switch (a)
             {
-                switch (data[i])
-                {
-                    case "x": print("x"); break;
-                    case "o": print("o"); break;
-                    case " ": print(" "); break;
-                }
-            } 
-            catch { break; }
+                case 1: data[t] = "x"; break;
+                case 2: data[t] = "o"; break;
+                case 3: data[t] = " "; break;
+            }
         }
-        Console.WriteLine();
     }
+    else
+    {
+
+        Thread.Sleep(200);
+        string a = " ; ; ; ; ; ; ; ; ";
+        data = a.Split(";");
+        realgame = true;
+
+    }
+
+        Console.Clear();
+
+    for (int z = 0; z < 4; z++, one += 3, two += 3, counter = 1)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("╠╬════════════╬════════════╬═══════════╬╣");
+        for (int x = 0; x < three; x++, counter += 1)
+        {
+            for (int i = one; i < two; i++)
+            {
+                try
+                {
+                    piece = i;
+                    switch (data[i])
+                    {
+                        case "x": print("x"); break;
+                        case "o": print("o"); break;
+                        case " ": print(" "); break;
+                    }
+                }
+                catch { break; }
+            }
+            Console.WriteLine();
+        }
+        if (three < 4) { Thread.Sleep(20); }
+    }
+}
+
+
+void menu(string str)
+{
+        switch (str)
+        {
+            case "input":
+            Console.WriteLine("╠╬════════════╣            ╠═══════════╬╣");
+            break;
+        }
 }
 
 void print(string str) 
@@ -63,13 +108,23 @@ secondcounter += 1;
             break;
 
             case " ":
-                    Console.Write(" ║           ");
+           
+                 if (counter==2&&realgame==true) 
+                    { 
+                    Console.Write(" ║"); 
+                    Console.ForegroundColor = ConsoleColor.Red; 
+                    Console.Write($"     {piece+1}     "); 
+                    Console.ForegroundColor = ConsoleColor.White; }
+                    else
+                    {
+                    Console.Write($" ║           ");
+                    }
                     print("backline");
             break;
 
             case "backline":
-            Console.ForegroundColor = ConsoleColor.White;
-            secondcounter -= 1;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    secondcounter -= 1;
                     if (secondcounter == 3) { secondcounter = 0; Console.Write("║"); }
             break;
         }
