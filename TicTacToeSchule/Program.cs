f2f445031f5e5576bc4cd48122e23e672e5c6572
Wabtyy <109748192+Wabtyy@users.noreply.github.com>
2025-02-28 11:40:48 +0100

    Update Program.cs

--- TicTacToeSchule/Program.cs
+++ TicTacToeSchule/Program.cs
@@ -1,103 +1,79 @@
-string rawdata = "O;X; ;X; ;X; ;O; ".ToLower();
+string rawdata = "o; ;o;o;x;x;X; ;O".ToLower();
 string[] data = rawdata.Split(";");
 
-string oo = "         ";
+string x1 = "   ═╗ ╔═ ",    o1 = "    ╔═╗  ";
+string x2 = "    ╚╦╝  ",    o2 = "   ╔╝ ╚╗ ";
+string x3 = "    ╔╩╗  ",    o3 = "   ╚╗ ╔╝ ";
+string x4 = "   ═╝ ╚═ ",    o4 = "    ╚═╝  ";
 
-string x1 = "   ═╗ ╔═ ";
-string x2 = "    ╚╦╝  ";
-string x3 = "    ╔╩╗  ";
-string x4 = "   ═╝ ╚═ ";
+int counter = 1, secondcounter = 0, one = 0, two = 3;
 
-string o1 = "    ╔═╗  ";
-string o2 = "   ╔╝ ╚╗ ";
-string o3 = "   ╚╗ ╔╝ ";
-string o4 = "    ╚═╝  ";
+//////////////////////////////////////////////////////
 
-int counter = 0;
-int secondcounter = 0;
-for (int x = 0; x < 4; x++)
+for (int z = 0; z < 4; z++, one+=3, two+=3, counter=1)
 {
-    counter += 1;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("╠╬════════════╬════════════╬═══════════╬╣");
 
-    for (int i = 0; i < data.Length/3; i++)
+    for (int x = 0; x < 4; x++, counter += 1)
     {
-        switch (data[i])
+        for (int i = one; i < two; i++)
         {
-            case "x": print("x"); break;
-            case "o": print("o"); break;
-            case " ": print(" "); break;
+            try
+            {
+                switch (data[i])
+                {
+                    case "x": print("x"); break;
+                    case "o": print("o"); break;
+                    case " ": print(" "); break;
+                }
+            } 
+            catch { break; }
         }
+        Console.WriteLine();
     }
-    Console.WriteLine();
 }
-counter = 0;
-Console.WriteLine("═══════════════════════════════════");
-for (int x = 0; x < 4; x++)
-{
-    counter += 1;
 
-    for (int i = data.Length / 3; i < data.Length/1.5; i++)
-    {
-        switch (data[i])
+void print(string str) 
+{ 
+secondcounter += 1;
+
+        switch (str)
         {
-            case "x": print("x"); break;
-            case "o": print("o"); break;
-            case " ": print(" "); break;
-        }
-    }
-    Console.WriteLine();
-}
-Console.WriteLine("═══════════════════════════════════"); 
-counter = 0;
-for (int x = data.Length / 2 + 1; x < data.Length; x++)
-{
-    counter += 1;
-
-    for (int i = 6; i < 9; i++)
-    {
-        switch (data[i])
-        {
-            case "x": print("x"); break;
-            case "o": print("o"); break;
-            case " ": print(" "); break;
-        }
-    }
-    Console.WriteLine();
-}
-Console.WriteLine("");
-
-
-
-
-string print(string str)
-{
-    secondcounter += 1;
-
-    switch (str)
-    {
-        case "x":
-            if (counter == 1) { Console.Write(x1 + "  "); }
-            if (counter == 2) { Console.Write(x2 + "  "); }
-            if (counter == 3) { Console.Write(x3 + "  "); }
-            if (counter == 4) { Console.Write(x4 + "  "); }
-            if (secondcounter != 3) { Console.Write("║"); } else { secondcounter = 0; }
+              case "x":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(" ║");
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    if (counter == 1) { Console.Write(x1 + "  "); }
+                    if (counter == 2) { Console.Write(x2 + "  "); }
+                    if (counter == 3) { Console.Write(x3 + "  "); }
+                    if (counter == 4) { Console.Write(x4 + "  "); }
+                    print("backline");
             break;
 
-        case "o":
-            if (counter == 1) { Console.Write(o1 + "  "); }
-            if (counter == 2) { Console.Write(o2 + "  "); }
-            if (counter == 3) { Console.Write(o3 + "  "); }
-            if (counter == 4) { Console.Write(o4 + "  "); }
-            if (secondcounter != 3) { Console.Write("║"); } else { secondcounter = 0; }
+            case "o":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(" ║");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    if (counter == 1) { Console.Write(o1 + "  "); }
+                    if (counter == 2) { Console.Write(o2 + "  "); }
+                    if (counter == 3) { Console.Write(o3 + "  "); }
+                    if (counter == 4) { Console.Write(o4 + "  "); }
+                    print("backline");
             break;
 
-        case " ":
-            if (counter == 1) { Console.Write("           "); }
-            if (counter == 2) { Console.Write("           "); }
-            if (counter == 3) { Console.Write("           "); }
-            if (counter == 4) { Console.Write("           "); }
-            if (secondcounter != 3) { Console.Write("║"); } else { secondcounter = 0; }
+            case " ":
+                    Console.Write(" ║           ");
+                    print("backline");
             break;
-    }
-    return "";
+
+            case "backline":
+            Console.ForegroundColor = ConsoleColor.White;
+            secondcounter -= 1;
+                    if (secondcounter == 3) { secondcounter = 0; Console.Write("║"); }
+            break;
+        }
 }
+
+    Thread.Sleep(200000);
+    Console.Clear();
